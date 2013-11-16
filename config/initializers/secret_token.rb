d9daa55b0ea831dbd0c936d0037926cdfd2813f4
cc <237178842@qq.com>
2013-11-16 17:16:07 +0800

    create a new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinanceStar::Application.config.secret_key_base = 'f97ab4c76b280a0b08278008b274b4d32df01e02bebffc887acd5f0a59e3bb224af0d57729c702006604b2fdfee9157792a06884d5182c82ba5348ec18f8c376'
