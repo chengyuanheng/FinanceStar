a6ef78d95504b050ddc854de2896b27081eaac57
cc <237178842@qq.com>
2013-12-21 06:43:07 +0800

    add change_style.js for web  header

---
+++ app/assets/javascripts/change_style.js
@@ -0,0 +1,42 @@
+//add  active class to header tag
+
+$(document).on("page:load", function () {
+    change_style_to_active()
+});
+
+$(document).ready(function () {
+    change_style_to_active()
+});
+
+
+function change_style_to_active() {
+    var id = fetch_id_base_path();
+
+    $("#" + id).addClass('active')
+}
+
+
+function fetch_id_base_path() {
+
+    var id_transform_from_path = _build_id();
+
+    var customer_mapping = {
+        _finance_management_index: '_finance_management_index',
+        _finance_management_customer_management_index: '_finance_management_customer_management_index',
+        _finance_management_my_finance_index: '_finance_management_my_finance_index',
+        _finance_management_finance_statistics_index: '_finance_management_finance_statistics_index',
+    };
+
+    var mapping = {};
+    $.extend(mapping, customer_mapping);
+
+    return mapping[id_transform_from_path];
+
+
+    function _build_id() {
+        var path = window.location.pathname;
+        return path.replace(/\//g, '_');
+    }
+}
+
+
